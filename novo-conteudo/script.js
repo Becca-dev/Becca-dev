8869c368fb36d61e1e7f9a550acb58c9edd15b63
Becca-dev <becca9399@gmail.com>
2022-11-20 17:04:34 -0300

    mundo-invertido.v1

---
+++ novo-conteudo/script.js
@@ -0,0 +1,10 @@
+/*  Faça um programa para calcular o valor de uma viagem.
+
+Voçê terá 3 variáveis. Sende elas:
+1 - Preço do combustível;
+2 - Gastos médio do combustivel do carro do KM,
+3 - Distancia em KM de viagem.
+
+*/
+
+console.log('Oiiiii')
